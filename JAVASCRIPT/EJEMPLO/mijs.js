26291bcc0d8e1ad9759d674ef9e049accc002fee
MariaMalaver <mariamalaver480@gmail.com>
2026-05-07 09:31:35 +0200

    javascript primera clase

---
+++ JAVASCRIPT/EJEMPLO/mijs.js
@@ -0,0 +1,7 @@
+var x = prompt("Introduce un numero");
+
+var suma = parseFloat(x) + parseFloat(x); // si ponemos parseFloat, o parseInt se sumara como una suma normal, pero si no lo ponemos se pone como una cadena emplan 5 + 5 = 55, y si lo ponemos es 5 + 5 = 10.
+
+//alert(suma);
+console.log(suma); // con el alert se muestra en una ventana emergente, y con el console.log se muestra en la consola del navegador. la suma aparecera hay.
+
