916561a3c848017755774d52173bf7c42ba9c0f9
MariaMalaver <mariamalaver480@gmail.com>
2026-05-13 11:13:30 +0200

    termianr esto pero es la primera practica de js

--- JAVASCRIPT/EJEMPLO/mijs.js
+++ JAVASCRIPT/EJEMPLO/mijs.js
@@ -1,7 +1,82 @@
+/* -- 1
 var x = prompt("Introduce un numero");
 
 var suma = parseFloat(x) + parseFloat(x); // si ponemos parseFloat, o parseInt se sumara como una suma normal, pero si no lo ponemos se pone como una cadena emplan 5 + 5 = 55, y si lo ponemos es 5 + 5 = 10.
 
 //alert(suma);
 console.log(suma); // con el alert se muestra en una ventana emergente, y con el console.log se muestra en la consola del navegador. la suma aparecera hay.
+*/
+
+// =======================================================================================================
+
+/** eston con onclick en el html -- 2 
+ * function saludar() {
+ *   alert("Hola, ¿Cómo estás?");
+ * }
+ */
+
+// =======================================================================================================
+
+// -- 3, es lo mismo qeu el dos pero desde javascrpt
+document.getElementById("botonSaludar").onclick = function() 
+{ 
+    alert("Hola");
+};
+
+/**
+ * TAMBIEN TENEMOS APARTE DEL ONCLICK ESTOS
+ * 
+ * 1. ONKEYDOWN: se ejecuta cuando se presiona una tecla.
+ * 2. ONKEYUP: se ejecuta cuando se suelta una tecla.
+ * 3. ONMOUSEENTER: se ejecuta cuando el puntero del mouse entra en un elemento.
+ * 4. ONCLICK: se ejecuta cuando se hace clic en un elemento.
+ * ETC...
+ * 
+ * HAY MUCHOS MAS, ESTOS SON SOLO ALGUNOS EJEMPLOS DE LOS EVENTOS QUE EXISTEN EN JAVASCRIPT.
+ */
+
+// =======================================================================================================
+
+// -- 4
+document.getElementById("botonCopiar").onclick = function() 
+{ 
+    var x = document.getElementById("origen").value;
+
+    document.getElementById("destino").value = x;
+};
+
+// =======================================================================================================
+
+// -- 5
+document.getElementById("botonTriple").onclick = function() 
+{ 
+    var x = document.getElementById("numero1").value;
+
+    /**
+     * parseFloat se utiliza para convertir el valor de x a un número decimal, 
+     * ya que por defecto los valores de los inputs son cadenas de texto.
+     */ 
+    document.getElementById("numero2").value = parseFloat(x) * 3; 
+
+};
+
+// =======================================================================================================
+
+// -- 6
+document.getElementById("botonTriple").onclick = function() 
+{ 
+    var x = document.getElementById("numero1").value;
+
+    if(x == "0")
+    {
+        alert("error")
+    }
+    else
+    {
+        document.getElementById("numero2").value = parseFloat(x) * 3;
+    }
+};
+
+// =======================================================================================================
+
 
---
+++ JAVASCRIPT/PRIMERA_PRACTICA/practica1.js
@@ -0,0 +1,39 @@
+function calcular(operacion) 
+{
+
+    var num1 = parseFloat(document.getElementById("operando1").value);
+    var num2 = parseFloat(document.getElementById("operando2").value);
+    var resultado = document.getElementById("resultado");
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultado.textContent = "Error introduce números correctos";
+        return;
+    }
+
+    var res;
+
+    if (operacion == "+") 
+    {
+        document.getElementById("numero2").value = parseFloat(num1) + parseFloat(num2);
+    } 
+    else if (operacion == "-") 
+    {
+        res = num1 - num2;
+    } 
+    else if (operacion == "*") 
+    {
+        res = num1 * num2;
+    } 
+    else if (operacion == "/") 
+    {
+
+        if (num2 == "0") 
+        {
+            resultado.textContent = "Error";
+            return;
+        }
+        res = num1 / num2;
+    }
+
+    resultado.textContent = "Resultado: " + res;
+}
